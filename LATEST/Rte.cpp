cf461fd668cc03af065c8ebd83d94f67cda16892
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:09:33 +0100

    Update: AUTOSAR basic software module Rte

--- LATEST/Rte.cpp
+++ LATEST/Rte.cpp
@@ -40,6 +40,16 @@ class module_Rte:
       FUNC(void, RTE_CODE) DeInitFunction (void);
       FUNC(void, RTE_CODE) GetVersionInfo (void);
       FUNC(void, RTE_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, RTE_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
