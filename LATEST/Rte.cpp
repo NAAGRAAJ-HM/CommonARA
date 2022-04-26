feae306f5573bdcede7e49af68708df30803dfcd
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-26 10:11:33 +0000

    Update: Generated RTE for AUTOSAR stack

--- LATEST/Rte.cpp
+++ LATEST/Rte.cpp
@@ -84,6 +84,10 @@ FUNC(void, RTE_CODE) module_Rte::InitFunction(
    if(E_OK == IsInitDone){
 #if(STD_ON == Rte_DevErrorDetect)
       Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
       );
 #endif
    }
@@ -92,6 +96,10 @@ FUNC(void, RTE_CODE) module_Rte::InitFunction(
       if(NULL_PTR == lptrCfgModule){
 #if(STD_ON == Rte_DevErrorDetect)
          Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
          );
 #endif
       }
@@ -116,6 +124,10 @@ FUNC(void, RTE_CODE) module_Rte::DeInitFunction(void){
    if(E_OK != IsInitDone){
 #if(STD_ON == Rte_DevErrorDetect)
       Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
       );
 #endif
    }
@@ -132,6 +144,10 @@ FUNC(void, RTE_CODE) module_Rte::MainFunction(void){
    if(E_OK != IsInitDone){
 #if(STD_ON == Rte_DevErrorDetect)
       Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
       );
 #endif
    }
