f60bc5b660b5c243123c2e95a7c1c67fa13b70a4
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:02:29 +0000

    Update: Generated RTE for AUTOSAR stack

--- LATEST/Rte.cpp
+++ LATEST/Rte.cpp
@@ -113,7 +113,7 @@ FUNC(void, RTE_CODE) module_Rte::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  RTE_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, RTE_CODE) module_Rte::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  RTE_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, RTE_CODE) module_Rte::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  RTE_E_UNINIT
       );
 #endif
    }
