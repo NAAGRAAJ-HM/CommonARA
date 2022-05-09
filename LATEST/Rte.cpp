f0cf661dfe2f04bad2c80d4f8ff1971c7d97e987
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:37:12 +0000

    Update: Generated RTE for AUTOSAR stack

--- LATEST/Rte.cpp
+++ LATEST/Rte.cpp
@@ -35,6 +35,13 @@ class module_Rte:
       public abstract_module
    ,  public class_Rte_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Rte(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
