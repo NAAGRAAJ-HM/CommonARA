6ee621a5a3ed2959f251d0ee3b23c9950db10f3f
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-03 17:37:41 +0200

    Update: Generated RTE for AUTOSAR stack

---
+++ BldcFocSensorless_LATEST/Rte.cpp
@@ -0,0 +1,145 @@
+/******************************************************************************/
+/* File   : Rte.cpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "Rte.hpp"
+#include "infRte_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define RTE_AR_RELEASE_VERSION_MAJOR                                           4
+#define RTE_AR_RELEASE_VERSION_MINOR                                           3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(RTE_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible RTE_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(RTE_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible RTE_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_Rte, RTE_VAR) Rte;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, RTE_CODE) module_Rte::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, RTE_CONST,       RTE_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   RTE_CONFIG_DATA, RTE_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == Rte_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstRte_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == Rte_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == Rte_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == Rte_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  RTE_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, RTE_CODE) module_Rte::DeInitFunction(
+   void
+){
+#if(STD_ON == Rte_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == Rte_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == Rte_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  RTE_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, RTE_CODE) module_Rte::MainFunction(
+   void
+){
+#if(STD_ON == Rte_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == Rte_InitCheck)
+   }
+   else{
+#if(STD_ON == Rte_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  RTE_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
