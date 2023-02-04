0a6b09aa4921376f9f9e68d5a1c15676a3dce661
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:27:50 +0100

    Update: Generated RTE for AUTOSAR stack

--- BldcFocSensorlessTLE9879QXA40_R2023/infProjectARA_ServiceNvM_Types.hpp
+++ BldcFocSensorlessTLE9879QXA40_R2023/infProjectARA_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ProjectARA_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ProjectARA_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
